06730fed44317d922da4b2c0552730870478291d
sogaiu <983021772@users.noreply.github.com>
2024-10-13 23:34:49 +0900

    Add folds.scm

---
+++ grammars/tree-sitter/folds.scm
@@ -0,0 +1,14 @@
+[
+  (par_arr_lit)
+  (sqr_arr_lit)
+  (struct_lit)
+  (tbl_lit)
+  (par_tup_lit)
+  (sqr_tup_lit)
+
+  (qq_lit)
+  (quote_lit)
+  (short_fn_lit)
+  (splice_lit)
+  (unquote_lit)
+] @fold
